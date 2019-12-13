bde1c4b5b910f59bea268fc43109a35bed7cba01
huajian <2876439672@qq.com>
2019-12-13 19:53:56 +0800

    修改登录页

--- lib/pages/home_page/home.dart
+++ lib/pages/home_page/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -37,6 +38,8 @@ class HomePage extends StatelessWidget {
 }
 
 class MyDrawer extends StatelessWidget {
+  final double _menuFontSize = ScreenUtil().setSp(30.0);
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
@@ -47,78 +50,86 @@ class MyDrawer extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Padding(
-              padding: EdgeInsets.only(top: 38.0),
-              child: Row(
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 16.0),
-                    child: ClipOval(
-                      child: Image.asset(
-                        "static/images/user.jpg",
-                        width: 80.0,
+                padding: EdgeInsets.only(top: 38.0),
+                child: Row(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 16.0),
+                      child: ClipOval(
+                        child: Image.asset(
+                          "static/images/user.jpg",
+                          width: 80.0,
+                        ),
                       ),
                     ),
-                  ),
-                  Text(
-                    "董事长",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  )
-                ],
-              )
-            ),
-
-
+                    Text(
+                      "董事长",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    )
+                  ],
+                )),
             Expanded(
               child: ListView(
                 children: <Widget>[
                   ListTile(
-                    leading: Icon(Icons.add),
-                    title: Text("添加"),
+                    title: Text(
+                      "问题反馈",
+                      style: TextStyle(fontSize: _menuFontSize),
+                    ),
                   ),
                   ListTile(
-                    leading: Icon(Icons.add),
-                    title: Text("添加"),
+                    title: Text(
+                      "阅读历史",
+                      style: TextStyle(fontSize: _menuFontSize),
+                    ),
                   ),
                   ListTile(
-                    leading: Icon(Icons.add),
-                    title: Text("添加"),
+                    title: Text(
+                      "个人信息",
+                      style: TextStyle(fontSize: _menuFontSize),
+                    ),
                   ),
                   ListTile(
-                    leading: Icon(Icons.add),
-                    title: Text("添加"),
+                    title: Text(
+                      "切换主题",
+                      style: TextStyle(fontSize: _menuFontSize),
+                    ),
                   ),
                   ListTile(
-                    leading: Icon(Icons.add),
-                    title: Text("添加"),
+                    title: Text(
+                      "语言切换",
+                      style: TextStyle(fontSize: _menuFontSize),
+                    ),
                   ),
                   ListTile(
-                    leading: Icon(Icons.add),
-                    title: Text("添加"),
+                    title: Text(
+                      "检测更新",
+                      style: TextStyle(fontSize: _menuFontSize),
+                    ),
                   ),
                   ListTile(
-                    leading: Icon(Icons.add),
-                    title: Text("添加"),
+                    title: Text(
+                      "关于",
+                      style: TextStyle(fontSize: _menuFontSize),
+                    ),
                   ),
-                  ListTile(
-                    leading: Icon(Icons.add),
-                    title: Text("添加"),
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.add),
-                    title: Text("添加"),
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.add),
-                    title: Text("添加"),
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.add),
-                    title: Text("添加"),
-                  ),
-                  ListTile(
-                    leading: Icon(Icons.exit_to_app),
-                    title: Text("退出"),
-                  )
+                  Container(
+                      width: ScreenUtil().setWidth(1.0),
+                      height: ScreenUtil().setHeight(90.0),
+                      child: Padding(
+                        padding: EdgeInsets.only(left: 15.0,right: 15.0),
+                        child: RaisedButton(
+                          color: Theme.of(context).primaryColor,
+                          child: Text(
+                            "退 出",
+                            style: TextStyle(fontSize: _menuFontSize,color: Colors.white),
+                          ),
+                          elevation: 2.0,
+                          onPressed: () {
+                            print("退出");
+                          },
+                        ),
+                      ))
                 ],
               ),
             )
@@ -128,3 +139,6 @@ class MyDrawer extends StatelessWidget {
     );
   }
 }
+
+/*
+,*/
