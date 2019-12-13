ee956e20333bc2f9625e86d53d1087512fa4d0e4
huajian <287643967@qq.com>
2019-12-13 16:21:19 +0800

    代码提交

--- lib/pages/home_page/home.dart
+++ lib/pages/home_page/home.dart
@@ -17,9 +17,9 @@ class HomePage extends StatelessWidget {
         ],
       ),
       drawer: Container(
-        child: Text("抽屉"),
+        child: MyDrawer(),
       ),
-     // floatingActionButton:
+      // floatingActionButton:
       bottomNavigationBar: BottomAppBar(
         color: Colors.white,
         shape: CircularNotchedRectangle(), // 底部导航栏打一个圆形的洞
@@ -33,5 +33,98 @@ class HomePage extends StatelessWidget {
         ),
       ),
     ));
+  }
+}
+
+class MyDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: MediaQuery.removePadding(
+        context: context,
+        removeTop: true,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(top: 38.0),
+              child: Row(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16.0),
+                    child: ClipOval(
+                      child: Image.asset(
+                        "static/images/user.jpg",
+                        width: 80.0,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    "董事长",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  )
+                ],
+              )
+            ),
+
+
+            Expanded(
+              child: ListView(
+                children: <Widget>[
+                  ListTile(
+                    leading: Icon(Icons.add),
+                    title: Text("添加"),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.add),
+                    title: Text("添加"),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.add),
+                    title: Text("添加"),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.add),
+                    title: Text("添加"),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.add),
+                    title: Text("添加"),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.add),
+                    title: Text("添加"),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.add),
+                    title: Text("添加"),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.add),
+                    title: Text("添加"),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.add),
+                    title: Text("添加"),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.add),
+                    title: Text("添加"),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.add),
+                    title: Text("添加"),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.exit_to_app),
+                    title: Text("退出"),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
