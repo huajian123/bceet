a73e8ee3b45653caf7a19d763615f8bf77112e90
huajian <287643967@qq.com>
2019-12-21 15:22:28 +0800

    添加列表页

--- lib/pages/home_page/home.dart
+++ lib/pages/home_page/home.dart
@@ -9,28 +9,95 @@ class HomePage extends StatelessWidget {
         child: Scaffold(
             body: Container(
               child: Column(
-                children: <Widget>[
-                  SwiperDiy(),
-                  NewsList(),
-                ],
-              ),
-            )
-        ));
+              children: <Widget>[
+                SwiperDiy(),
+                NewsList(),
+              ],
+      ),
+    )));
   }
 }
 
 class NewsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Expanded(
       child: ListView(
-
+        shrinkWrap: true,
+        padding: const EdgeInsets.all(10.0),
+        children: <Widget>[
+          NewsItem(),
+          NewsItem(),
+          NewsItem(),
+          NewsItem(),
+          NewsItem(),
+          NewsItem(),
+          NewsItem(),
+          NewsItem(),
+          NewsItem(),
+          NewsItem(),
+          NewsItem(),
+          NewsItem(),
+        ],
       ),
     );
+
+//    return  Column(
+//      children: <Widget>[
+//        NewsItem(),
+//        NewsItem(),
+//        NewsItem(),
+//      ],
+//    );
   }
 }
 
-
+class NewsItem extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Text(
+                  '继山东编导艺考联考被曝疑似出现泄题和作弊的情况。江西编导艺考联考也被曝疑似出现泄题和作弊的情况。',
+                  style: TextStyle(
+                    fontSize: 15.0,
+                    color: Color(0xff111111),
+                  ),
+                  maxLines: 3,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 10.0,
+            ),
+            Container(
+              height: 85.0,
+              width: 115.0,
+              margin: EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(5.0),
+                  image: DecorationImage(
+                      image: AssetImage("static/images/news1.jpeg"),
+                      fit: BoxFit.cover)),
+            ),
+          ],
+        ),
+        Container(
+          color: Color(0xffeaeaea),
+          constraints: BoxConstraints.expand(height: 4.0),
+        )
+      ],
+    );
+  }
+}
 
 // 轮播组件
 class SwiperDiy extends StatelessWidget {
@@ -43,9 +110,12 @@ class SwiperDiy extends StatelessWidget {
     return Container(
       height: ScreenUtil().setHeight(400.0),
       child: Swiper(
-        itemBuilder: (BuildContext context, int index){
-          print(index);
-          return Image.asset("static/images/swiper${index}.png",fit: BoxFit.fill,);
+        autoplay: false,
+        itemBuilder: (BuildContext context, int index) {
+          return Image.asset(
+            "static/images/swiper${index}.png",
+            fit: BoxFit.fill,
+          );
         },
         pagination: SwiperPagination(),
         itemCount: 5,
