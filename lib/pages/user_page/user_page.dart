9cad2dccaf79557f62ec240e93f89290f2848f2c
huajian <287643967@qq.com>
2019-12-24 13:46:15 +0800

    添加我的页面塞贝尔曲线

--- lib/pages/user_page/user_page.dart
+++ lib/pages/user_page/user_page.dart
@@ -5,22 +5,97 @@ class UserPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: ClipPath(
-          // 绘制塞贝二曲线
+        body: Column(
+      children: <Widget>[
+        ClipPath(
+            // 绘制塞贝二曲线
             clipper: BottomClipper(),
             // 对最小宽度进行限制，限制为宽度为100%
             child: ConstrainedBox(
-              constraints: BoxConstraints(
-                minWidth: double.infinity
-              ),
+              constraints: BoxConstraints(minWidth: double.infinity),
               child: Container(
-                height: ScreenUtil().setHeight(500.0),
-                decoration: BoxDecoration(
-                  color: Theme.of(context).primaryColor,
-                ),
-                child: Text('123'),
+                  height: ScreenUtil().setHeight(500.0),
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                  ),
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        ClipOval(
+                          child: Image.asset(
+                            "static/images/user_avatar.png",
+                            height: ScreenUtil().setHeight(180.0),
+                          ),
+                        ),
+                        FlatButton(
+                          child: Text(
+                            "点击登录",
+                            style: TextStyle(
+                                fontSize: ScreenUtil().setSp(40.0),
+                                color: Colors.white),
+                          ),
+                          onPressed: () {},
+                        )
+                      ],
+                    ),
+                  )),
+            )),
+        Container(
+          child: Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all()
               ),
-            )));
+              child: ListView(
+                children: <Widget>[
+                  ListTile(
+                    leading: Icon(Icons.settings),
+                    title: Text("收藏"),
+                    trailing: Icon(Icons.keyboard_arrow_right),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.settings),
+                    title: Text("收藏"),
+                    trailing: Icon(Icons.keyboard_arrow_right),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.settings),
+                    title: Text("收藏"),
+                    trailing: Icon(Icons.keyboard_arrow_right),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.settings),
+                    title: Text("收藏"),
+                    trailing: Icon(Icons.keyboard_arrow_right),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.settings),
+                    title: Text("收藏"),
+                    trailing: Icon(Icons.keyboard_arrow_right),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.settings),
+                    title: Text("收藏"),
+                    trailing: Icon(Icons.keyboard_arrow_right),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.settings),
+                    title: Text("收藏"),
+                    trailing: Icon(Icons.keyboard_arrow_right),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.settings),
+                    title: Text("收藏"),
+                    trailing: Icon(Icons.keyboard_arrow_right),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        )
+      ],
+    ));
   }
 }
 
